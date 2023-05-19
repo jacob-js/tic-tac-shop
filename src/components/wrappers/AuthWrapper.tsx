e3a5d7347fe99b002224aa116ed181784f87babd
Jacob Dev <mercihabam@gmail.com>
2023-05-19 15:32:53 +0200

    feat: create authwrapper component

---
+++ src/components/wrappers/AuthWrapper.tsx
@@ -0,0 +1,22 @@
+import { PropsWithChildren } from "react"
+
+function AuthWrapper({title, subtitle, children}: PropsWithChildren<{title: string, subtitle?: string}>) {
+  return (
+    <div className="w-full min-h-screen bg-white flex justify-center items-center">
+    <div className="flex flex-col sm:flex-row rounded-lg shadow-light">
+        <div className="bg-primary rounded-t-lg sm:rounded-s-lg sm:rounded-tr-none p-5 flex items-center justify-center">
+            <div className="text-white font-bold">Tic-tac Shop</div>
+        </div>
+        <div className="bg-white p-10 sm:px-20 rounded-b-lg sm:rounded-e-lg">
+            <div className="flex flex-col gap-2 mb-10 text-gray-500">
+                <div className="text-2xl font-bold">{title}</div>
+                <div>{subtitle}</div>
+            </div>
+            {children}
+        </div>
+    </div>
+</div>
+  )
+}
+
+export default AuthWrapper
--- src/pages/authentication/Login.tsx
+++ src/pages/authentication/Login.tsx
@@ -1,41 +1,29 @@
 import { Input, Form, Button } from 'antd';
+import { AuthWrapper } from '../../components';
 
 function Login() {
   return (
-    <div className="w-full min-h-screen bg-white flex justify-center items-center">
-        <div className="flex flex-col sm:flex-row rounded-lg shadow-light">
-            <div className="bg-primary rounded-t-lg sm:rounded-s-lg sm:rounded-tr-none p-5 flex items-center justify-center">
-                <div className="text-white font-bold">Tic-tac Shop</div>
-            </div>
-            <div className="bg-white p-10 sm:px-20 rounded-b-lg sm:rounded-e-lg">
-                <div className="flex flex-col gap-2 mb-10 text-gray-500">
-                    <div className="text-2xl font-bold">Welcome</div>
-                    <div className="">Please login to your account to continue</div>
+    <AuthWrapper title='Welcome' subtitle='Please login to your account to continue'>
+        <Form
+            layout='vertical'
+        >
+            <Form.Item name="email" label="Email" required>
+                <Input placeholder='ex: johndoe@gmail.com' size='large' />
+            </Form.Item>
+            <Form.Item name="password" label="Password" required>
+                <Input.Password placeholder='****' size='large' />
+            </Form.Item>
+            <Form.Item>
+                <Button type='primary' block size='large'>Login</Button>
+            </Form.Item>
+            <Form.Item>
+                <div className="flex justify-between">
+                    <Button type='link'>Reset password</Button>
+                    <Button type='link'>Register</Button>
                 </div>
-                <div className="">
-                    <Form
-                        layout='vertical'
-                    >
-                        <Form.Item name="email" label="Email" required>
-                            <Input placeholder='ex: johndoe@gmail.com' size='large' />
-                        </Form.Item>
-                        <Form.Item name="password" label="Password" required>
-                            <Input.Password placeholder='****' size='large' />
-                        </Form.Item>
-                        <Form.Item>
-                            <Button type='primary' block size='large'>Login</Button>
-                        </Form.Item>
-                        <Form.Item>
-                            <div className="flex justify-between">
-                                <Button type='link'>Reset password</Button>
-                                <Button type='link'>Register</Button>
-                            </div>
-                        </Form.Item>
-                    </Form>
-                </div>
-            </div>
-        </div>
-    </div>
+            </Form.Item>
+        </Form>
+    </AuthWrapper>
   )
 }
 
