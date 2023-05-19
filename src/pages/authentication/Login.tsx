f9207ef338da4b814bfdad178308a4a0ee7a5d13
Jacob Dev <mercihabam@gmail.com>
2023-05-19 15:13:13 +0200

    feat: make it responsive

--- src/pages/authentication/Login.tsx
+++ src/pages/authentication/Login.tsx
@@ -3,11 +3,11 @@ import { Input, Form, Button } from 'antd';
 function Login() {
   return (
     <div className="w-full min-h-screen bg-white flex justify-center items-center">
-        <div className="flex rounded-2xl shadow-light">
-            <div className="bg-primary rounded-s-lg p-5 flex items-center justify-center">
+        <div className="flex flex-col sm:flex-row rounded-lg shadow-light">
+            <div className="bg-primary rounded-t-lg sm:rounded-s-lg sm:rounded-tr-none p-5 flex items-center justify-center">
                 <div className="text-white font-bold">Tic-tac Shop</div>
             </div>
-            <div className="bg-white p-10 px-20 rounded-e-lg">
+            <div className="bg-white p-10 sm:px-20 rounded-b-lg sm:rounded-e-lg">
                 <div className="flex flex-col gap-2 mb-10 text-gray-500">
                     <div className="text-2xl font-bold">Welcome</div>
                     <div className="">Please login to your account to continue</div>
