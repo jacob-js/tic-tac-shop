9883f6d1c2f23e84ecf42d9e41510c36cf251d04
Jacob Dev <mercihabam@gmail.com>
2023-05-19 11:37:11 +0200

    feat: add ant design

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import 'antd/dist/reset.css';
 import './index.css'
+import { ConfigProvider } from 'antd';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <ConfigProvider
+      theme={{
+        token: {
+          colorPrimary: "#dd4900"
+        }
+      }}
+    >
+      <App />
+    </ConfigProvider>
   </React.StrictMode>,
 )
